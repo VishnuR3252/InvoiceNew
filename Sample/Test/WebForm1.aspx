9b4eb94a4a5596e4b7b5e90109f32583ba6c3889
100698 <100698@MACTECH.LOC>
2024-07-09 16:55:10 +0530

    initial commit

---
+++ Sample/Test/WebForm1.aspx
@@ -0,0 +1,124 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Sample.Test.WebForm1" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Sample Form</title>
+
+</head>
+<body>
+    <form id="form1" runat="server" style="background-color:azure">
+        <%--<div>
+            <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+            <br />
+            <asp:DropDownList ID="DropDownList2" runat="server"></asp:DropDownList>
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        </div>--%>
+        <div class="content">
+            <div class="MainContent"> 
+                <div class="row">
+                    <div class="col-md-10 header animated bounceIn" style="z-index: 1">
+
+                        <h2>Billing System</h2>
+
+
+                    </div>
+
+
+                </div> 
+                <%-- nav bar tab --%>
+                
+              <br/>
+                <div class="tab-content">
+                      <%-- NormalRelease --%> 
+                    
+                        <div class="form-group">
+                            
+
+                                <label class="col-md-2 cntr-text">Select Customer:<span class="required text-danger">*</span></label>
+                                 <asp:DropDownList ID="DropDownList1" runat="server" style="width: 15%;">
+                                     <asp:ListItem Selected="True">--Select--</asp:ListItem>
+                                 </asp:DropDownList>&nbsp&nbsp
+                                
+                            
+
+                                <label class="col-md-2 cntr-text">Select Product:<span class="required text-danger">*</span> </label>
+                                <asp:DropDownList ID="DropDownList2" runat="server" style="width: 15%;">
+                                    <asp:ListItem Selected="True">--Select--</asp:ListItem>
+                                </asp:DropDownList>
+
+                            
+                            </div><br />
+                        <div class="form-group">
+                            
+                                <label id="T1" class="col-md-2 cntr-text">Quantity: </label>
+                                <asp:TextBox ID="TextBox1" runat="server" style="width: 15%;"></asp:TextBox>&nbsp&nbsp
+
+                             
+                                <label id="T2" class="col-md-2 cntr-text">Tax: </label>
+                                <asp:TextBox ID="TextBox2" runat="server" style="width: 15%;" ReadOnly="True">15</asp:TextBox>
+
+                                 
+                            <label id="T3" class="col-md-2 cntr-text">Price: </label>
+
+                                <asp:TextBox ID="TextBox3" runat="server" style="width: 15%;"></asp:TextBox>
+
+                            
+                            </div><br />
+                            
+                        
+                            
+
+                        
+
+                </div><br />
+
+
+
+                        
+                            <div class="col-md-6">
+
+                                
+                                <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+                                <br />
+                                <br />
+                                
+                                <asp:Button ID="Button2" runat="server" Text="Clear" OnClick="Button2_Click" />
+                                
+                            </div>
+                        
+
+                        </div>
+                       
+                        
+                    </div>
+
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true"></asp:GridView>
+
+                    <%-- NormalRelease --%>
+                    
+
+
+
+
+                    <%-- Body End --%>
+                
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Untaxed Amount"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Tax 15%"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Total"></asp:Label>
+        </p>
+                
+    </form>
+</body>
+</html>
---
+++ Sample/Test/WebForm1.aspx.cs
@@ -0,0 +1,213 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sample.Test
+{
+    public partial class WebForm1 : Page
+    {
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString;
+        int taxex = 0;
+        int id = 1;
+        private DataTable DataTable
+        {
+            get
+            {
+                if (ViewState["DataTable"] == null)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Columns.Add("Customer");
+                    dt.Columns.Add("Product");
+                    dt.Columns.Add("Quantity");
+                    dt.Columns.Add("Tax");
+                    dt.Columns.Add("Price");
+                    dt.Columns.Add("TaxExc");
+                    ViewState["DataTable"] = dt;
+                }
+                return (DataTable)ViewState["DataTable"];
+            }
+            set { ViewState["DataTable"] = value; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Populate dropdowns on initial load
+                PopulateCustomerDropdown();
+                PopulateProductDropdown();
+            }
+        }
+
+        //protected void PopulateCustomerDropdown()
+        //{
+        //    string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString; string query = "SELECT Id, Name FROM Employees";
+          
+        //    using (SqlConnection conn = new SqlConnection(connectionString))
+        //    {
+
+        //        SqlCommand cmd = new SqlCommand(query, conn);
+        //        conn.Open();
+
+        //        SqlDataReader reader = cmd.ExecuteReader();
+        //        DropDownList1.DataSource = reader;
+        //        DropDownList1.DataTextField = "Name";
+        //        DropDownList1.DataValueField = "Id";
+        //        DropDownList1.DataBind();
+
+        //        reader.Close();
+        //    }
+        //}
+
+
+        protected void PopulateCustomerDropdown()
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString; string query = "SELECT Id, Name FROM Customers";
+
+            // -- added postgresql
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+
+                NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+                conn.Open();
+
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                DropDownList1.DataSource = reader;
+                DropDownList1.DataTextField = "Name";
+                DropDownList1.DataValueField = "Id";
+                DropDownList1.DataBind();
+
+                reader.Close();
+            }
+        }
+
+        protected void PopulateProductDropdown()
+        {
+             string query = "SELECT pid, pname FROM Products";
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+                conn.Open();
+
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                DropDownList2.DataSource = reader;
+                DropDownList2.DataTextField = "pname";
+                DropDownList2.DataValueField = "pid";
+                DropDownList2.DataBind();
+
+                reader.Close();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string selectedValue1 = DropDownList1.SelectedItem.Text;
+            string selectedValue2 = DropDownList2.SelectedItem.Text;
+            string textBox1Value = TextBox1.Text;
+            string textBox2Value = TextBox2.Text;
+            string textBox3Value = TextBox3.Text;
+
+             taxex = Convert.ToInt32(TextBox1.Text) * Convert.ToInt32(TextBox3.Text);
+            // Add the values to the DataTable
+            DataRow row = DataTable.NewRow();
+            row["Customer"] = selectedValue1;
+            row["Product"] = selectedValue2;
+            row["Quantity"] = textBox1Value;
+            row["Tax"] = textBox2Value;
+            row["Price"] = textBox3Value;
+            row["TaxExc"] = taxex;
+            DataTable.Rows.Add(row);
+
+            // Bind the DataTable to the GridView
+            BindGridView();
+        }
+
+        private void BindGridView()
+        {
+            GridView1.DataSource = DataTable;
+            GridView1.DataBind();
+
+            
+            string que = "SELECT MAX(id) FROM Invoicebill";
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                NpgsqlCommand cmd = new NpgsqlCommand(que, conn);
+                conn.Open();
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                if(id == 1)
+                {
+                    id = 1;
+                }
+                else
+                {
+                    id = id + 1;
+
+                }
+                reader.Close();
+            }
+
+            string qry = "insert into Invoicebill(id,Custid,pid,qty,price,taxexcl)values(" + id + "," + DropDownList1.SelectedValue + "," + DropDownList2.SelectedValue + "," + TextBox1.Text + "," + TextBox3.Text + "," + taxex + ")";
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                NpgsqlCommand cmd = new NpgsqlCommand(qry, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            CalculateAndDisplaySums(DataTable);
+        }
+
+        private void CalculateAndDisplaySums(DataTable dt)
+        {
+            // Initialize sums for each column
+            int untaxed = 0;
+            int tax = 0;
+            int total = 0;
+            
+
+            // Iterate through each row in the DataTable to calculate sums
+            foreach (DataRow row in dt.Rows)
+            {
+                untaxed += Convert.ToInt32(row["TaxExc"]);
+                
+            }
+
+            tax = untaxed * 15 / 100;
+            total = untaxed + tax;
+
+            // Display or use the sums as needed
+            Label1.Text = "Untaxed Amount: " + untaxed.ToString();
+            Label2.Text = "Tax 15% : " + tax.ToString();
+            Label3.Text = "Total : " + total.ToString();
+
+
+            string qryyy = "insert into InvoiceMaster(id,tax,untaxedamt,totaltax,totalamount)values(" + id + ",15,"+ untaxed + "," + tax + "," + total + ")";
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                NpgsqlCommand cmd = new NpgsqlCommand(qryyy, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            //Label1.Text = "";
+            //Label2.Text = "";
+            //Label3.Text = "";
+        
+        }
+    }
+}
